3b90cf6cc978c2e56e93438432ead3bb8d376dfa
Christian Puhrsch <cpuhrsch@devfair0129.h2.fair>
2019-07-19 15:05:30 -0700

    cleaner

--- references/classification/train.py
+++ references/classification/train.py
@@ -57,6 +57,7 @@ except ImportError:
 
 def train_one_epoch(model, criterion, optimizer, data_loader, device, epoch, print_freq, traindir, executor, apex=False):
     # data with futures
+    traindir = os.path.join(args.data_path, 'train')
     classes, class_to_idx = torchvision.datasets.DatasetFolder._find_classes(traindir)
     files = torchvision.datasets.folder.make_dataset(traindir, class_to_idx, extensions=torchvision.datasets.folder.IMG_EXTENSIONS)
     random.shuffle(files) # torch.utils.data.RandomSampler
@@ -102,6 +103,7 @@ def train_one_epoch(model, criterion, optimizer, data_loader, device, epoch, pri
 
 
 def evaluate(model, criterion, data_loader, device):
+    valdir = os.path.join(args.data_path, 'val')
     model.eval()
     metric_logger = utils.MetricLogger(delimiter="  ")
     header = 'Test:'
@@ -155,64 +157,8 @@ def main(args):
 
     # Data loading code
     print("Loading data")
-    traindir = os.path.join(args.data_path, 'train')
-    valdir = os.path.join(args.data_path, 'val')
     normalize = transforms.Normalize(mean=[0.485, 0.456, 0.406],
                                      std=[0.229, 0.224, 0.225])
-
-    print("Loading training data")
-    st = time.time()
-    cache_path = _get_cache_path(traindir)
-    if args.cache_dataset and os.path.exists(cache_path):
-        # Attention, as the transforms are also cached!
-        print("Loading dataset_train from {}".format(cache_path))
-        dataset, _ = torch.load(cache_path)
-    else:
-        dataset = torchvision.datasets.ImageFolder(
-            traindir,
-            my_transforms)
-        if args.cache_dataset:
-            print("Saving dataset_train to {}".format(cache_path))
-            utils.mkdir(os.path.dirname(cache_path))
-            utils.save_on_master((dataset, traindir), cache_path)
-    print("Took", time.time() - st)
-
-    print("Loading validation data")
-    cache_path = _get_cache_path(valdir)
-    if args.cache_dataset and os.path.exists(cache_path):
-        # Attention, as the transforms are also cached!
-        print("Loading dataset_test from {}".format(cache_path))
-        dataset_test, _ = torch.load(cache_path)
-    else:
-        dataset_test = torchvision.datasets.ImageFolder(
-            valdir,
-            transforms.Compose([
-                transforms.Resize(256),
-                transforms.CenterCrop(224),
-                transforms.ToTensor(),
-                normalize,
-            ]))
-        if args.cache_dataset:
-            print("Saving dataset_test to {}".format(cache_path))
-            utils.mkdir(os.path.dirname(cache_path))
-            utils.save_on_master((dataset_test, valdir), cache_path)
-
-    print("Creating data loaders")
-    if args.distributed:
-        train_sampler = torch.utils.data.distributed.DistributedSampler(dataset)
-        test_sampler = torch.utils.data.distributed.DistributedSampler(dataset_test)
-    else:
-        train_sampler = torch.utils.data.RandomSampler(dataset)
-        test_sampler = torch.utils.data.SequentialSampler(dataset_test)
-
-    data_loader = torch.utils.data.DataLoader(
-        dataset, batch_size=args.batch_size,
-        sampler=train_sampler, num_workers=args.workers, pin_memory=True)
-    data_loader = len(data_loader) * [(None, None)]
-
-    data_loader_test = torch.utils.data.DataLoader(
-        dataset_test, batch_size=args.batch_size,
-        sampler=test_sampler, num_workers=args.workers, pin_memory=True)
 
     print("Creating model")
     model = torchvision.models.__dict__[args.model](pretrained=args.pretrained)
